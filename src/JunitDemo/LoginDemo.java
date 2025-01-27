ba42a8d384f8efc42b8f70fb9d22e370f51bf8b6
Admin <amolujagare@gmail.com>
2025-01-27 20:47:28 +0530

    selenium

---
+++ src/JunitDemo/LoginDemo.java
@@ -0,0 +1,28 @@
+package JunitDemo;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginDemo {
+
+    @Test
+    public  void loginTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://stock.scriptinglogic.in/");
+
+        WebElement txtUsername = driver.findElement(By.xpath("//input[@id='login-username']"));
+        txtUsername.sendKeys("admin");
+
+        WebElement txtPassword = driver.findElement(By.xpath("//input[@id='login-password']"));
+        txtPassword.sendKeys("admin");
+
+        WebElement btnLogin = driver.findElement(By.xpath("//input[@value='LOG IN']"));
+        btnLogin.click();
+
+    }
+}
